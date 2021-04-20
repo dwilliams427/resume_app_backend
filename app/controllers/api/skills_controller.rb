bd736b8cf10ac99c42c637f6087305ff6252e140
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-04-20 13:47:02 -0400

    Makes CRUD for SKILLS controller, no user-id

--- app/controllers/api/skills_controller.rb
+++ app/controllers/api/skills_controller.rb
@@ -1,2 +1,44 @@
 class Api::SkillsController < ApplicationController
+  def index
+    @skills = Skill.all
+    render "index.json.jb"
+  end
+
+  def show
+    skill_id = params[:id]
+    @skill = Skill.find(skill_id)
+    render "show.json.jb"
+  end
+
+  def create
+    @skill = Skill.new(
+      skill_name: params[:skill_name],
+      # student_id: current_user.id,
+    )
+    if @skill.save
+      render "show.json.jb"
+    else
+      render json: { error: @skill.errors.full_messages }
+    end
+  end
+
+  def update
+    skill_id = params[:id]
+    @skill = Skill.find_by(id: skill_id)
+
+    @skill.skill_name = params[:skill_name] || @skill.skill_name
+
+    if @skill.save
+      render "show.json.jb"
+    else
+      render json: { error: @skill.errors.full_messages }
+    end
+  end
+
+  def destroy
+    skill_id = params[:id]
+    skill = Skill.find_by(id: skill_id)
+    skill.destroy
+    render json: { message: "This skill was successfully deleted." }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+
+    #Skills Model
+    get "skills" => "skills#index"
+    post "skills/" => "skills#create"
+    get "skills/:id" => "skills#show"
+    patch "skills/:id" => "skills#update"
+    delete "skills/:id" => "skills#destroy"
+  end
 end
