6e2f4301f89f2f6b452ade81ff9873faf3cce042
Daniel Williams <dlw1ll27@gmail.com>
2021-04-20 09:41:38 -0700

    adds education model routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    #EDUCATION ROUTES
+    get "/education", controller: "education", action: "index"
+    get "/education/:id", controller: "education", action: "show"
+    post "/education", controller: "education", action: "create"
+    patch "/education/:id", controller: "education", action: "update"
+    delete "/education/:id", controller: "education", action: "destroy"
+  end
 end
