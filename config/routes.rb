2335fde9b3694adcddbab6c1bbffad65d0ef1242
Mark Francisco <mfrancisco581@gmail.com>
2021-04-20 12:46:45 -0500

    Add routes, controller actions, views for Experience model. Authentication functionality is commented out

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,23 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+
+
+
+
+
+
+
+
+
+
+
+    
+    get "/experiences" => "experiences#index"
+    get "/experiences/:id" => "experiences#show"
+    post "/experiences" => "experiences#create"
+    patch "/experiences/:id" => "experiences#update"
+    delete "/experiences/:id" => "experiences#destroy"
+  end
 end
