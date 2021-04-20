b000cd0c5aaffe00730f34c907da5b1ae34e1780
Daniel Williams <dlw1ll27@gmail.com>
2021-04-20 09:47:33 -0700

    adds education model and view files and starter code

--- app/controllers/api/education_controller.rb
+++ app/controllers/api/education_controller.rb
@@ -1,3 +1,58 @@
 class Api::EducationController < ApplicationController
-  
+  before_action :authenticate_admin, except: [:index, :show]
+
+  def index
+    @actor = Actor.all.order(age: :asc)
+    render "index.json.jb"
+  end
+
+  def show_actor
+    actor_id = params[:id]
+    @actor = Actor.find(actor_id)
+    render "show_actor.json.jb"
+  end
+
+  def create
+    @actor = Actor.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      known_for: params[:known_for],
+      gender: params[:gender],
+      age: params[:age],
+      movie_id: params[:movie_id],
+    )
+    #happy/sad path
+    if @actor.save
+      render "show_actor.json.jb"             #happy path
+    else
+      render json: { errors: @actor.errors.full_messages }, status: 406                       #sad path
+    end
+  end
+
+  def update
+    actor_id = params[:id]
+    @actor = Actor.find_by(id: actor_id)
+
+    @actor.first_name = params[:first_name] || @actor.first_name
+    @actor.last_name = params[:last_name] || @actor.last_name
+    @actor.known_for = params[:known_for] || @actor.known_for
+    @actor.gender = params[:gender] || @actor.gender
+    @actor.movie_id = params[:movie_id] || @actor.movie_id
+    @actor.age = params[:age] || @actor.age
+
+    #happy/sad path
+    if @actor.save
+      render "show_actor.json.jb"             #happy path
+    else
+      render json: { errors: @actor.errors.full_messages }, status: 406                       #sad path
+    end
+  end
+
+  def destroy
+    actor_id = params[:id]
+    @actor = Actor.find_by(id: actor_id)
+
+    @actor.destroy
+    render json: { message: "deleted actor" }
+  end
 end
