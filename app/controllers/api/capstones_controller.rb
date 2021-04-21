beb9700cfb65e9f36fd6fb369d0f0f4eebf06d50
Stacie Taitague <stacie.taitague@gmail.com>
2021-04-21 13:39:20 -0400

    fixes

--- app/controllers/api/capstones_controller.rb
+++ app/controllers/api/capstones_controller.rb
@@ -1,4 +1,6 @@
 class Api::CapstonesController < ApplicationController
+  before_action :authenticate_user, except: [:index, :show]
+
   def index
     @capstones = Capstone.all
     render "index.json.jb"
